3a5a1759dde80e725dd98b649b041d5b7614d925
redheart76 <redheart76@gmail.com>
2022-11-27 18:04:26 +1300

    first commit

---
+++ src/test/java/UI/SearchTest.java
@@ -0,0 +1,127 @@
+package UI;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SearchTest {
+	    private WebDriver driver ;
+	    private String BaseUrl= "https://www.trademe.co.nz";
+	   
+
+@BeforeMethod
+	public void BeforeClass() {
+	    driver = new ChromeDriver();
+		driver.get(BaseUrl);
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+	
+@AfterClass
+ public void CloseBrowser() {
+	  driver.close();
+}
+
+	@Test
+		public void SearchFerrari() throws InterruptedException {
+		      //driver.get(BaseUrl);
+			  driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-dynamic-homepage/tm-homepage-search-header/nav/div[2]/ul/li[3]/a")).click();
+		      Thread.sleep(5000);
+		      WebElement DropDown = driver.findElement(By.name("selectedMake"));
+		      Select select = new Select(DropDown);
+		      select.selectByValue(("Ferrari"));
+		      Thread.sleep(5000);
+
+		      driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-home-page/tm-motors-home-page-header/div/div/tm-motors-search-form/form/tm-motors-used-cars-large/tg-row[3]/tg-col[3]/button")).click();
+		      Thread.sleep(5000);
+
+		      WebElement SearchResult1 = driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-search-results/div/div/div[1]/div/tm-search-header-result-count"));
+		      WebElement MakeDropdown = driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-search-results/tm-search-header/div/div/tm-refine-header/div[4]/tm-tags/tm-drop-down-tag[4]/tg-tag/span/button"));
+		      String ExpectMake = "Make: Ferrari";
+		      //To verify "Ferrari" is displayed in the Make dropdown menu after searching
+		      Assert.assertEquals(MakeDropdown.getText(),ExpectMake);
+		      System.out.println("Ferrari was found and the test passed");
+		      //To get and print out the search result
+		      String ActualResult = SearchResult1.getText();
+		      System.out.println(ActualResult);  
+		  }
+	 @Test
+	  public void SearchBMW() throws InterruptedException {
+	      //driver.get(BaseUrl);
+		  driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-dynamic-homepage/tm-homepage-search-header/nav/div[2]/ul/li[3]/a")).click();
+	      Thread.sleep(5000);
+	      WebElement DropDown = driver.findElement(By.name("selectedMake"));
+	      Select select = new Select(DropDown);
+	      select.selectByValue(("BMW"));
+	      Thread.sleep(5000);
+
+	      driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-home-page/tm-motors-home-page-header/div/div/tm-motors-search-form/form/tm-motors-used-cars-large/tg-row[3]/tg-col[3]/button")).click();
+	      Thread.sleep(5000);
+
+	      WebElement SearchResult1 = driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-search-results/div/div/div[1]/div/tm-search-header-result-count"));
+	      WebElement MakeDropdown = driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-search-results/tm-search-header/div/div/tm-refine-header/div[4]/tm-tags/tm-drop-down-tag[4]/tg-tag/span/button"));
+	      String ExpectMake = "Make: BMW";
+	      //To verify "Ferrari" is displayed in the Make dropdown menu after searching
+	      Assert.assertEquals(MakeDropdown.getText(),ExpectMake);
+	      System.out.println("BMW was found and the test passed");
+	      //To get and print out the search result
+	      String ActualResult = SearchResult1.getText();
+	      System.out.println(ActualResult); 
+	      
+	}
+	 @Test
+	  public void SearchHonda() throws InterruptedException {
+	      driver.get(BaseUrl);
+		  driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-dynamic-homepage/tm-homepage-search-header/nav/div[2]/ul/li[3]/a")).click();
+	      Thread.sleep(5000);
+	      WebElement DropDown = driver.findElement(By.name("selectedMake"));
+	      Select select = new Select(DropDown);
+	      select.selectByValue(("Honda"));
+	      Thread.sleep(5000);
+
+	      driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-home-page/tm-motors-home-page-header/div/div/tm-motors-search-form/form/tm-motors-used-cars-large/tg-row[3]/tg-col[3]/button")).click();
+	      Thread.sleep(5000);
+
+	      WebElement SearchResult1 = driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-search-results/div/div/div[1]/div/tm-search-header-result-count"));
+	      WebElement MakeDropdown = driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-search-results/tm-search-header/div/div/tm-refine-header/div[4]/tm-tags/tm-drop-down-tag[4]/tg-tag/span/button"));
+	      String ExpectMake = "Make: Honda";
+	      //To verify "Ferrari" is displayed in the Make dropdown menu after searching
+	      Assert.assertEquals(MakeDropdown.getText(),ExpectMake);
+	      System.out.println("Honda was found and the test passed");
+	      //To get and print out the search result
+	      String ActualResult = SearchResult1.getText();
+	      System.out.println(ActualResult);  
+	}
+	  
+	  @Test
+	  public void SearchMazda() throws InterruptedException {
+	      driver.get(BaseUrl);
+		  driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-dynamic-homepage/tm-homepage-search-header/nav/div[2]/ul/li[3]/a")).click();
+	      Thread.sleep(5000);
+	      WebElement DropDown = driver.findElement(By.name("selectedMake"));
+	      Select select = new Select(DropDown);
+	      select.selectByValue(("Mazda"));
+	      Thread.sleep(5000);
+
+	      driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-home-page/tm-motors-home-page-header/div/div/tm-motors-search-form/form/tm-motors-used-cars-large/tg-row[3]/tg-col[3]/button")).click();
+	      Thread.sleep(5000);
+
+	      WebElement SearchResult1 = driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-search-results/div/div/div[1]/div/tm-search-header-result-count"));
+	      WebElement MakeDropdown = driver.findElement(By.xpath("/html/body/tm-root/div[1]/main/div/tm-motors-search-results/tm-search-header/div/div/tm-refine-header/div[4]/tm-tags/tm-drop-down-tag[4]/tg-tag/span/button"));
+	      String ExpectMake = "Make: Mazda";
+	      //To verify "Ferrari" is displayed in the Make dropdown menu after searching
+	      Assert.assertEquals(MakeDropdown.getText(),ExpectMake);
+	      System.out.println("Mazda was found and the test passed");
+	      //To get and print out the search result
+	      String ActualResult = SearchResult1.getText();
+	      System.out.println(ActualResult);  
+	}
+	}
+
+
